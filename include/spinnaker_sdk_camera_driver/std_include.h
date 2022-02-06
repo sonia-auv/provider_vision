cca6e63a5e0a9cdccbeb1f8fbd43941a59e92689
supertoto29 <francisalonzo29@gmail.com>
2022-02-06 21:11:10 +0000

    update to opencv 4.2.0 Fix include highgui_c.h

--- include/spinnaker_sdk_camera_driver/std_include.h
+++ include/spinnaker_sdk_camera_driver/std_include.h
@@ -8,7 +8,7 @@
 // OpenCV
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/highgui/highgui_c.hpp>
+#include <opencv2/highgui/highgui_c.h>
 
 // ROS
 #include <ros/ros.h>
